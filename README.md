0e8996a3b4e4855bd77da0f164f136afd146c16f
kwi5i <32113899+kiwi5@users.noreply.github.com>
2017-10-01 13:23:29 +0200

    removed the eval hack and improved readablility
    Improved the 'spcInput()' function.
    Cleaned the mess around the input sequence.
    Renamed a few functions and variables.
    
    Edited readme.md a bit.

--- README.md
+++ README.md
@@ -1,30 +1,29 @@
 # poe-40-py
 
-"Just take these numbers and return best combinations summing up to 40".
+### "Just take these numbers between 1 & 19, and return best combinations summing up to 40".
 
-This is for the purpose of finding an effective way to combine looted items, needed for ()[Quality Recipe] in Path of Exile game.
+This is for the purpose of finding an effective way to combine looted items, needed for [Quality Recipe](https://pathofexile.gamepedia.com/Vendor_recipe_system#Quality_Items) in Path of Exile game.
 Saves the time, as counting it in calculator (or mentally, as some are able to) is no longer needed.
-
-Only after running this script have I gotten to know HOW MANY more combinations there could be, that I hadn't suspected.
 
 
 How to use:
  1. Open the script.
- 2. Write procent values, separated with spaces, of all items of a given type you'd like to vendor. Press Enter.
- 3. Any (such,enclosed) values make up a single 40% recipe.
-
-It needs Python 3 installed on your machine to work. Python 2 shouldn't work.
-Tested it on 3.6.2 only.
+ 1. Write procent values of all items of a given type you'd like to vendor.
+ 1. Script outputs what combinations are available to you.
 
 
-This is what I consider my very first program, even though this isn't strictly true.
+> NOTE: It needs Python 3 installed on your machine to work. Python 2 shouldn't work.
+> Tested it on 3.6.2 only.
+
+Only after running this script have I gotten to know _how many_ more combinations there could be, that I hadn't even suspected.
+
+This is what I consider my very first complete program, although there have been a few tiny ones before.
 
 
 ====================================
 Todo here:
-   - read about Markup and format this document better
-   - give example of input and output
-   - note some case use bug(s?)
-   - some other stuff I've forgotten
-   - 
-   - 
+   - [x] read about Markdown and format this document better
+   - [ ] note some case-use bugs
+   - [ ] give example of input and output?
+   - [ ] some other stuff I've forgotten
+   - [ ] 
--- poe 40.py
+++ poe 40.py
@@ -2,8 +2,8 @@
 # BONUS: window always on top
 # BONUS: if compatible file's present --> read data from it w/o asking
 # BONUS: reading from file & FURTHER manual reading - and saving the whole afterwards, for later (must be able to subtract from what's already there)
-# TODO: shorten loops using list comprehension, so it'd only ever be adding what's =40, at all
-# TODO: if 'kombinZ()' won't find ANY combination --> print this info & cease operation
+# TODO: shorten loops using list comprehension + so it'd only ever be adding what's =40, at all
+# TODO: if 'kombin()' won't find ANY combination --> print this info & cease operation
 
 
 # BONUS: additional features & things that may become TODOs once current ones are dealt with
@@ -14,32 +14,34 @@
 # ============================= IMPORTS: ===============================
 from itertools import combinations
 from re import sub
+from sys import exit
 
 # ============================ FUNCTIONS: ==============================
 def spcInput():
-   '''Asks for integer input: returns reversely sorted list of values <1,20>.'''
-   # input works, but is UNSAFE to use, due to 'eval()' being applied
+   '''Asks for integer input: returns reversely sorted list of values (1-19).'''
+   print("Input values separated with spaces or comas and press Enter:")
    while True:
       try:   
-         # DEBUG: tabl = eval("[" + sub("(^[\D]+)|([\D]+$)","",sub("[\D]+",",",str("6 18 14 9 12 10 10 10 9 5 8 2 10"))) + "]") #.replace(" ",",")
-         # DEBUG: tabl = eval("[" + sub("(^[\D]+)|([\D]+$)","",sub("[\D]+",",",str(" 43 5 -565-43-78_32+32,65,32  ,456 +_,64"))) + "]") #.replace(" ",",")
-
-         tabl = eval("[" + sub("(^[\D]+)|([\D]+$)","",sub("[\D]+",",",input("Input values separated with space or coma: \n> "))) + "]")
+         # TEST: str("6 18 14 9 12 10 10 10 9 5 8 2 10")
+         # TEST: str(" 43 5 -565-43-78_32+32,65,32  ,456 +_,64")
+         tabl = input("> ")
+         tabl = sub("(^[\D]+)|([\D]+$)","",sub("[\D]+",",",tabl))
          # allows nothing besides digits at beginning/end of input
          #  and changes any other non-digits in-between to comas
-         # TODO: find a simpler regex for this
+         tabl = tabl.split(",")
+         tabl = [int(a) for a in tabl]
          print("Raw:  %r" % tabl)
       except (NameError, SyntaxError):
-         print("Incorrect input: I only take numbers, spaces and comas.")
+         # didn't test how these catch now..
+         print("Incorrect input: I take only numbers and spaces or comas.")
          continue
-      for n in tabl[:]:
-         if n > 20 or n < 1:
-            tabl.remove(n)
+      
+      [tabl.remove(n) for n in tabl[:] if n >= 19 or n <= 1]
       break
    tabl.sort(reverse=True)
    return tabl
 
-def kombinZ(proc_array_, arr=[(40,)]):
+def kombin(perc_array_, arr=[(40,)]):
    '''Returns all combinations summing up to 40, sorted up, \nand prints them in lines.'''
    # BUG: if there're *possible* TWO IDENTICAL combinations, then it treats one as duplicate!
    ## eg. '10 10 10 10 10 10 10 10' (that's 2x40 combos, and as such should be returned)
@@ -48,13 +50,13 @@ def kombinZ(proc_array_, arr=[(40,)]):
    # TODO.META:   1. de-duplicate after/while generation
    ##             2. the 'and' from the 'if' below & non-empty initial list --> are not necessary
    ##             3. <here was supposed to BE something, perhabs...>
-   proc_array = proc_array_[:]
-   proc_array.sort(reverse=True)
+   perc_array = perc_array_[:]
+   perc_array.sort(reverse=True)
    # arr=[(40,)]    # non-empty, so that in 'if' below it doesn't go 'IndexError'
    # BUG: w/o resetting the 'arr=..' above, the function behaves as if initiated with values it had at its end in the previous loop
    ## I didn't expect Python to do that...
-   for x in range(3,len(proc_array)):      # from 3, bc only 2*20 == 40
-      LT = list(combinations(proc_array, x))      # alien code!
+   for x in range(3,len(perc_array)):      # from 3, bc only 2*20 == 40
+      LT = list(combinations(perc_array, x))      # alien code!
       LT.sort(reverse=True)
          # so that dups are adjacent
          # TODO: change this to de-dupl. w/o sorting + adjust the 'if' below
@@ -73,6 +75,7 @@ def kombinZ(proc_array_, arr=[(40,)]):
    print("Raw combinations: ")
    if arr == [(40,)]:
       print(" No matches found.")
+      print()
    else:
       # TODO: simplify printout, so it doesn't duplicate by itself (it's so for sorting by amount)    # ??
       # TODO: don't print entries with less elements than max (bc list can easily get lenghty)
@@ -83,11 +86,11 @@ def kombinZ(proc_array_, arr=[(40,)]):
       printout.sort(reverse=True)
       for a in printout:
          print("%2d %r" % (printout.index(a), a))
-      print("\n")
+      print()
    #
    return arr
 
-def kombi_opt(list0_, ref):
+def kombiOpt(list0_, ref):
    # TODO: turn this f() into a class (for more flexible printing, among others)
    ##       w/ method to printout elements sorted by their amount in "pair"
    list0 = list0_[:]
@@ -121,6 +124,7 @@ def kombi_opt(list0_, ref):
    print("Optimal combinations: ")
    if arr == []:
       print(" No matches found.")
+      print()
    else:
       printout = []
       for b in arr:
@@ -131,34 +135,41 @@ def kombi_opt(list0_, ref):
       printout.sort(reverse=True)
       #
       # '''printout only of the top level, sorted by number of groups (thanks to "len(b)")'''
+      print("   [item_count, group_count, (groups)")
       number = printout[0][0]
       for a in printout:
          if a[0] >= number:
             print("%2d %r" % (printout.index(a), a))
          else:
+            print()
             break
    #
    # TODO: print sequence that '''prints only the single top result'''
    return arr
 
-# BONUS: 
-#  def write_best():
+# TODO: 
+#  def printBest():
 #     list only combos of highest item count
-#     ==  ommit entries of kombi_opt of count lower than the highest
-#     AND if kombi_opt is empty --> list from kombinZ
+#     ==  ommit entries of kombiOpt of count lower than the highest
+#     AND if kombiOpt is empty --> list best from kombi
+
+def checkAgain():
+     '''Returns True if the user wants to check again, else it returns False.'''
+     print("Do you want to check again? (y/n)")
+     # BONUS: Enter/Esc
+     return input("> ").lower().startswith("y")
 
 # =============================== KOD: =================================
-while 1:
-   proc = spcInput()
-   print("Sorted:\n   %r %r \n" % (len(proc), proc))
-   if sum(proc) < 40:
-      print("That's not enough for recipe, loot some more.")
+while True:
+   perc = spcInput()
+   print("Sorted:\n   %r %r \n" % (len(perc), perc))
+   # TODO: always same indent, regardless of item count
+   if sum(perc) < 40:
+      print("That's not enough for the recipe, you need to loot some more.")
    else:
-      kombi_opt(kombinZ(proc,[(40,)]), proc)          
-      # w/o the "[(40,)]" the loop goes awry..
-      # this is necessary bc the functions don't seem to start "from zero" next time they're called in Python (or at least the way I wrote them..?)
-   input("\n\nPress ctrl-c to stop and exit.\n")      # I know, I knoooow...
-   # TODO: 'Esc' ends d'program (== ctrl-c?) "Press Esc to exit, Enter to cont."
-
-# TODO: cmprTimes(kombin+uniqL, kombinZ)
+      kombiOpt(kombin(perc,[(40,)]), perc)          
+      # w/o the explicit "[(40,)]" the loop goes awry..
+      # this is bc the functions don't seem to start "from zero" next time they're called in Python (or at least the way I wrote them..?)
+   if not checkAgain():
+      exit()
 input()
